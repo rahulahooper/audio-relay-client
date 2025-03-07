e4387ffb23b7ca6ab97dc81fae746efba2d32790
rahulahooper <shiv.rahul1@gmail.com>
2025-03-07 09:13:09 -0800

    Initial pass at sampling task and task synchronization

--- main/udp_client.c
+++ main/udp_client.c
@@ -50,15 +50,26 @@
 #define EXAMPLE_H2E_IDENTIFIER ""
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
 
-static const char *payload = "Hello from PC";
+#define ESP_CORE_0  0       // physical core 0
+#define ESP_CORE_1  1       // physical core 1
+#define PAYLOAD_MAX_LEN 996
 
-#define PAYLOAD_LEN 996
-struct AudioPacket_t
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
+typedef struct AudioPacket_t
 {
     uint16_t seqnum;
-    uint8_t  payload[PAYLOAD_LEN];
     uint16_t checksum;
-};
+    uint16_t payloadSize;
+    uint8_t  payload[PAYLOAD_MAX_LEN];
+} AudioPacket_t;
+
+
+typedef struct ResponsePacket_t
+{
+    uint16_t seqnum;
+    char     response[5];   // large enough to hold the "NACK" string
+} ResponsePacket_t;
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -74,6 +85,7 @@ static const char *TAG = "wifi station";
 static int s_retry_num = 0;
 
 esp_err_t wifi_station_start_and_connect(wifi_config_t* wifi_config, bool* error);
+
 
 
 ////////////////////////////////////////////////////////////////////
@@ -123,8 +135,9 @@ esp_err_t set_system_time()
     // TODO: Verify that we are connected to a Wifi network before connecting
     //       to the NTP server
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
-    config.start = false;   // wait for Wifi connection before starting SNTP service
+    config.start = false;               // wait for Wifi connection before starting SNTP service
     config.server_from_dhcp = true;     // use DHCP server to get SNTP IP address
+    config.smooth_sync = true;          // gradually reduce time error
     ESP_ERROR_CHECK(esp_netif_sntp_init(&config));
 
     ESP_LOGI(TAG, "%s Starting SNTP client\n", __func__);
@@ -132,10 +145,11 @@ esp_err_t set_system_time()
 
     time_t now = 0;
     struct tm timeinfo = { 0 };
-    if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK)
+    esp_err_t ret = esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000));
+    if (ret != ESP_OK)
     {
-        ESP_LOGE(TAG, "Failed to update system time within 10s timeout!\n");
-        return -1;
+        ESP_LOGE(TAG, "%s: Failed to update system time within 10s timeout! error = %s\n", __func__, esp_err_to_name(ret));
+        return ESP_OK;
     }
 
     time(&now);
@@ -170,122 +184,6 @@ void get_system_time(int64_t* time_us)
     *time_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
 }
 
-////////////////////////////////////////////////////////////////////
-// udp_client_task
-//
-// Main event loop for streaming data to server.
-////////////////////////////////////////////////////////////////////
-static void udp_client_task(void *pvParameters)
-{
-    char rx_buffer[128];
-    int addr_family = 0;
-    int ip_protocol = 0;
-
-    // I guess we're using the default one?
-    esp_netif_t* esp_netif = esp_netif_get_default_netif();
-    esp_netif_ip_info_t* ip_info = malloc(sizeof(esp_netif_ip_info_t));
-    if (!esp_netif || !ip_info)
-    {
-        ESP_LOGE(TAG, "%s Got a nullptr: esp_netif=%p, ip_info=%p\n", __func__, esp_netif, ip_info);
-        vTaskDelete(NULL);
-        return;
-    }
-
-    ESP_ERROR_CHECK( esp_netif_get_ip_info(esp_netif, ip_info) );
-    struct in_addr ipv4_addr;
-    char ipv4_str[INET_ADDRSTRLEN];
-    ipv4_addr.s_addr = ip_info->gw.addr;
-    if (inet_ntop(AF_INET, &ipv4_addr, ipv4_str, INET_ADDRSTRLEN) != NULL)
-    {
-
-        ESP_LOGI(TAG, "Gateway IPv4: %s", ipv4_str);
-    }
-
-    while (1) {
-
-#if defined(CONFIG_EXAMPLE_IPV4)
-        struct sockaddr_in dest_addr;
-        dest_addr.sin_addr.s_addr = inet_addr(ipv4_str);
-        dest_addr.sin_family = AF_INET;
-        dest_addr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-#elif defined(CONFIG_EXAMPLE_IPV6)
-        struct sockaddr_in6 dest_addr = { 0 };
-        inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
-        dest_addr.sin6_family = AF_INET6;
-        dest_addr.sin6_port = htons(PORT);
-        dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
-        addr_family = AF_INET6;
-        ip_protocol = IPPROTO_IPV6;
-#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-        struct sockaddr_storage dest_addr = { 0 };
-        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
-#endif
-
-        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-
-        // Set timeout
-        struct timeval timeout;
-        timeout.tv_sec = 10;
-        timeout.tv_usec = 0;
-        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
-
-        ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
-
-        uint16_t seqnum = 0;
-        int64_t timesent, timerecv;
-        while (1) {
-
-            struct AudioPacket_t packet;
-            packet.seqnum = seqnum++;
-            packet.checksum = crc16(packet.payload, PAYLOAD_LEN);
-
-            get_system_time(&timesent);
-            int err = sendto(sock, &packet, sizeof(packet), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                break;
-            }
-            ESP_LOGI(TAG, "Message sent");
-
-            struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
-            socklen_t socklen = sizeof(source_addr);
-            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
-
-            
-            if (len < 0) {          // Error occurred during receiving
-                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
-                break;
-            }
-            else {                  // Data received
-                get_system_time(&timerecv);
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "%s Received %d bytes from %s:", __func__, len, ipv4_str);
-
-                struct sockaddr_in * source_addr_in = (struct sockaddr_in *)&source_addr;
-                ESP_LOGI(TAG, "%s Source address: %s\n", __func__, inet_ntoa(source_addr_in->sin_addr));
-                ESP_LOGI(TAG, "%s Message content: %s", __func__, rx_buffer);
-
-
-                ESP_LOGI(TAG, "%s Round trip time: %lld\n", __func__, timerecv - timesent);
-            }
-
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
-        }
-
-        if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
-    }
-    vTaskDelete(NULL);
-}
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -435,6 +333,223 @@ esp_err_t wifi_station_disconnect_and_stop()
     return ret;
 }
 
+static AudioPacket_t gAudioPackets[2];
+static AudioPacket_t * activePacket;   // transmitting task transmits this packet
+static AudioPacket_t * backgroundPacket; // sampling task fills this packet
+
+static const UBaseType_t transmissionDoneNotifyIndex = 0;      // set by the transmission thread when it is done transmitting data
+static const UBaseType_t dataReadyNotifyIndex;                 // set by the sampling thread when there is new data to transmit
+
+static TaskHandle_t samplingTaskHandle = NULL;
+static TaskHandle_t transmitTaskHandle = NULL;
+
+
+////////////////////////////////////////////////////////////////////
+// sampling_task 
+//
+// Main event loop for sampling audio data
+////////////////////////////////////////////////////////////////////
+void sampling_task()
+{
+    activePacket = &gAudioPackets[0];
+    backgroundPacket = &gAudioPackets[1];
+
+    const uint32_t SAMPLE_RATE = 44100; // hz
+    const uint32_t SAMPLE_PERIOD_MS = (uint32_t)(1.0f / SAMPLE_RATE / 1000);
+
+    while (transmitTaskHandle == NULL)
+    {
+        ESP_LOGI(TAG, "%s: Waiting for transmit task to be created...\n", __func__);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+
+    uint16_t seqnum = 0;
+
+    while (true)
+    {
+
+        backgroundPacket->seqnum = ++seqnum;
+        backgroundPacket->checksum = 0;
+        backgroundPacket->payloadSize = 0;
+
+        uint32_t i = 0;
+
+        // keep sampling until the transmit thread is done sending the active packet
+        while (!ulTaskNotifyTakeIndexed(transmissionDoneNotifyIndex, pdTRUE, 0))
+        {
+            backgroundPacket->payload[i] = (uint8_t)(random() & 0xFF);
+            backgroundPacket->payloadSize = min(backgroundPacket->payloadSize+1, PAYLOAD_MAX_LEN);
+            i = (i+1) % PAYLOAD_MAX_LEN;
+
+            // sampling should probably be interrupt driven to allow for precise timing.
+            // this thread can just block until a sample is ready / all samples are ready.
+            // for now just sleep
+            vTaskDelay(SAMPLE_PERIOD_MS / portTICK_PERIOD_MS);
+        }
+
+        ESP_LOGI(TAG, "%s Transmission Done!\n", __func__);
+        // At this point, the transmit thread is waiting for a data ready signal and is
+        // therefore not sending any data. It is safe to swap the active and background
+        // packets.
+        AudioPacket_t * tmp = activePacket;
+        activePacket = backgroundPacket;
+        backgroundPacket = tmp;
+
+        ESP_LOGI(TAG, "%s Swapped buffers, notifying transmission task...\n", __func__);
+        xTaskNotifyGiveIndexed(transmitTaskHandle, dataReadyNotifyIndex);
+
+        // Q: How do we ensure that the sampling thread doesn't run through this loop multiple times before
+        //      the xmitter thread gets time to run?
+    }
+
+    vTaskDelete(NULL);
+}
+
+
+////////////////////////////////////////////////////////////////////
+// transmit_task 
+//
+// Main event loop for streaming data to server.
+////////////////////////////////////////////////////////////////////
+static void transmit_task(void *pvParameters)
+{
+    struct ResponsePacket_t response;
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    // I guess we're using the default one?
+    esp_netif_t* esp_netif = esp_netif_get_default_netif();
+    esp_netif_ip_info_t* ip_info = malloc(sizeof(esp_netif_ip_info_t));
+    if (!esp_netif || !ip_info)
+    {
+        ESP_LOGE(TAG, "%s Got a nullptr: esp_netif=%p, ip_info=%p\n", __func__, esp_netif, ip_info);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    ESP_ERROR_CHECK( esp_netif_get_ip_info(esp_netif, ip_info) );
+    struct in_addr ipv4_addr;
+    char ipv4_str[INET_ADDRSTRLEN];
+    ipv4_addr.s_addr = ip_info->gw.addr;
+    if (inet_ntop(AF_INET, &ipv4_addr, ipv4_str, INET_ADDRSTRLEN) != NULL)
+    {
+
+        ESP_LOGI(TAG, "Gateway IPv4: %s", ipv4_str);
+    }
+
+    while (samplingTaskHandle == NULL)
+    {
+        ESP_LOGI(TAG, "%s Waiting for sampling task to be created...\n", __func__);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+
+    while (1) {
+
+#if defined(CONFIG_EXAMPLE_IPV4)
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = inet_addr(ipv4_str);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_IPV6)
+        struct sockaddr_in6 dest_addr = { 0 };
+        inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
+        dest_addr.sin6_family = AF_INET6;
+        dest_addr.sin6_port = htons(PORT);
+        dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+        addr_family = AF_INET6;
+        ip_protocol = IPPROTO_IPV6;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_storage dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+
+        // Set timeout
+        struct timeval timeout;
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+        ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
+
+        int64_t timesent, timerecv;
+
+        while (1) {
+
+            // Indicate to sampling thread that there is not data to transmit
+            ESP_LOGI(TAG, "%s Notifying of transmission done.\n", __func__);
+            xTaskNotifyGiveIndexed(samplingTaskHandle, transmissionDoneNotifyIndex);
+
+            // Wait for the sampling task to indicate that there is new data available
+            ESP_LOGI(TAG, "%s Waiting for data ready.\n", __func__);
+            if(ulTaskNotifyTakeIndexed(dataReadyNotifyIndex, pdTRUE, pdMS_TO_TICKS(1000)))
+            {
+                ESP_LOGI(TAG, "%s Got data ready notification.\n", __func__);
+            }
+            else
+            {
+                ESP_LOGE(TAG, "%s Timed out waiting for data ready notification\n", __func__);
+                continue;
+            }
+
+            // CRC and transmit the audio packet
+            activePacket->checksum = crc16(activePacket->payload, activePacket->payloadSize);
+            uint32_t packetSize = sizeof(AudioPacket_t) - (PAYLOAD_MAX_LEN - activePacket->payloadSize);
+            get_system_time(&timesent);
+
+            ESP_LOGI(TAG, "%s Sending packet with payload len %u and CRC 0x%x to server.\n",
+                __func__, activePacket->payloadSize, activePacket->checksum);
+            int err = sendto(sock, activePacket, packetSize, 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                break;
+            }
+            ESP_LOGI(TAG, "Message sent");
+
+            // Receive ACK from server (this can be deleted later)
+            struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, &response, sizeof(struct ResponsePacket_t) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+            
+            if (len < 0) {          // Error occurred during receiving
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            else {                  // Data received
+                get_system_time(&timerecv);
+                response.response[4] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "%s Received %d bytes from %s:", __func__, len, ipv4_str);
+
+                struct sockaddr_in * source_addr_in = (struct sockaddr_in *)&source_addr;
+                ESP_LOGI(TAG, "%s Source address: %s\n", __func__, inet_ntoa(source_addr_in->sin_addr));
+                ESP_LOGI(TAG, "%s Message content: %s", __func__, response.response);
+
+
+                ESP_LOGI(TAG, "%s Round trip time: %lld\n", __func__, timerecv - timesent);
+            }
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
@@ -447,6 +562,9 @@ void app_main(void)
     #ifdef CONFIG_LWIP_DHCP_GET_NTP_SRV
     ESP_LOGI(TAG, "%s LWIP config'd\n", __func__);
     #endif
+
+    uint32_t num_cores = configNUM_CORES;
+    ESP_LOGI(TAG, "%s Number of cores: %lu\n", __func__, num_cores);
 
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -511,6 +629,23 @@ void app_main(void)
         },
     };
 
+    // TODO: This needs to go in a try-loop in case the client comes up
+    //       before the server, or in case the server powers down, then
+    //       comes back up.
     wifi_station_start_and_connect(&audio_wifi_config, &error);
-    xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
+    BaseType_t status = xTaskCreatePinnedToCore(transmit_task, "transmit_task", 4096, NULL, 5, &transmitTaskHandle, ESP_CORE_0);
+
+    if (status != pdPASS)
+    {
+        ESP_LOGE(TAG, "%s Failed to create transmit task!\n", __func__);
+        return;
+    }
+
+    status = xTaskCreatePinnedToCore(sampling_task, "sampling_task", 4096, NULL, 5, &samplingTaskHandle, ESP_CORE_1);
+
+    if (status != pdPASS)
+    {
+        ESP_LOGE(TAG, "%s Failed to create sampling task!\n", __func__);
+        return;
+    }
 }
